619aa441332a59bcf63f3a0fe7244ea908be243d
PatilPrasadS <prsyy1234@gmail.com>
2023-12-31 12:38:58 +0530

    Custom style Directive

---
+++ src/app/custom-style.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomStyle]',
+  standalone: true
+})
+export class CustomStyleDirective {
+
+  constructor(private obj : ElementRef) { 
+    this.obj.nativeElement.style.backgroundColor = "yellow";
+    this.obj.nativeElement.style.fontWeight = "bold";
+  }
+
+}
